a93d41e2864bdd852561fb926d4e86288ba9a8c8
Lucero Amaolo <lamaolo.m@gmail.com>
2021-10-03 18:09:16 -0300

    Optimizacion de imagenes

--- components/ProjectImage.jsx
+++ components/ProjectImage.jsx
@@ -1,23 +1,33 @@
-/* eslint-disable @next/next/no-img-element */
+import Image from "next/image";
 import styles from "../styles/ProjectImage.module.css";
 
 const ProjectImage = ({ imageToShow, name, pictures, handleImageClick }) => {
   return (
     <section className={styles.projectImage}>
       <div className={styles.imageContainer}>
-        <img src={imageToShow} alt={name} />
+        <Image
+          width={1920}
+          height={1080}
+          layout="responsive"
+          src={imageToShow}
+          alt={name}
+        />
       </div>
       <div className={styles.imageSelector}>
         {pictures.map((picture, i) => (
-          <div
-            key={i}
-            onClick={() => handleImageClick(picture)}
-            className={`${styles.imagePreview} ${
+          <article
+            className={`${styles.imageWrapper} ${
               imageToShow === picture && styles.selectedImg
             }`}
+            key={i}
           >
-            <img src={picture} alt={name} />
-          </div>
+            <div
+              onClick={() => handleImageClick(picture)}
+              className={styles.imagePreview}
+            >
+              <Image layout="fill" src={picture} alt={name} />
+            </div>
+          </article>
         ))}
       </div>
     </section>
