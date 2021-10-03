9b9b17dc5512d8de8c69189875b5deae53e8091b
Lucero Amaolo <lamaolo.m@gmail.com>
2021-10-03 19:33:22 -0300

    SEO optimizations

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import { useEffect, useState } from "react";
 
 import Header from "../components/Header";
@@ -15,6 +16,41 @@ function MyApp({ Component, pageProps }) {
 
   return (
     <>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta charSet="utf-8" />
+        <meta name="title" content="Lucero Amaolo" />
+        <meta
+          name="description"
+          content="Hey! I'm Lucero Amaolo, a front-end developer. This is my personal portfolio. Here you can see my work and contact me. "
+        />
+        <meta
+          name="keywords"
+          content="lucero Amaolo, lamaolo, github, frontend developer, designer"
+        />
+        <meta name="robots" content="index, follow" />
+        <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="language" content="English" />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="/seo-bg.png" />
+        <meta property="og:title" content="Lucero Amaolo" />
+        <meta
+          property="og:description"
+          content="Hey! I'm Lucero Amaolo, a front-end developer. This is my personal portfolio. Here you can see my work and contact me."
+        />
+        <meta property="og:image" content="/seo-bg.png" />
+
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://metatags.io/" />
+        <meta property="twitter:title" content="Lucero Amaolo" />
+        <meta
+          property="twitter:description"
+          content="Hey! I'm Lucero Amaolo, a front-end developer. This is my personal portfolio. Here you can see my work and contact me."
+        />
+        <meta property="twitter:image" content="/seo-bg.png" />
+      </Head>
       <Header isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
       <Component {...pageProps} />
       {isMenuOpen ? <Menu setIsMenuOpen={setIsMenuOpen} /> : null}
