c3ff606f43dc16205f0139ab9a89a32b13f9dca5
Lucero Amaolo <lamaolo.m@gmail.com>
2021-10-03 21:57:28 -0300

    Configuraciones de CSP

--- next.config.js
+++ next.config.js
@@ -1,16 +1,20 @@
 module.exports = {
   reactStrictMode: true,
-   async headers() {
-    return [
-      {
-        source: '/',
-        headers: [
-          {
-            key: 'connect-src',
-            value: 'self',
-          },
-        ],
-      },
-    ]
-  },
-}
+  // async headers() {
+  //   return [
+  //     {
+  //       source: "/",
+  //       headers: [
+  //         {
+  //           key: "connect-src",
+  //           value: "self",
+  //         },
+  //         {
+  //           key: "X-Robots-Tag",
+  //           value: "all",
+  //         },
+  //       ],
+  //     },
+  //   ];
+  // },
+};
